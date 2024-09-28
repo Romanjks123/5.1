2583fbe55a92e1ae6fcdaa6aee2a9e7456125058
Romanjks123 <m7playyt@gmail.com>
2024-09-28 19:47:49 +0300

    test

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = f(2);
+			Assert::AreEqual(t, 2);
+		}
+	};
+}
