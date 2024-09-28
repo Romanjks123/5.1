11277bff39b51429c31236b2f9b250ceb9d45cd7
Romanjks123 <m7playyt@gmail.com>
2024-09-28 19:04:45 +0300

    first commit

---
+++ 5.1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f(const double a);
+
+int main() {
+	double x, y;
+
+	cout << "x=  "; cin >> x;
+	cout << "y=  "; cin >> y;
+
+	double c = (f(3) + f(x + 1) + 1) / (pow(f(y + 1), 2));
+
+	cout << "c= " << c << endl;
+
+	return 0;
+}
+
+double f(const double a) {
+	return (a * a + 1) / (pow(sin(a), 2) + 1);
+}
